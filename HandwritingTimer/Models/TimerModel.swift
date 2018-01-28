4a690a2d8f01f68395134c80d1356773b627a44f
murakamiii <9khc727ru7@gmail.com>
2018-01-28 21:53:24 +0900

    UI調整

---
+++ HandwritingTimer/Models/TimerModel.swift
@@ -0,0 +1,9 @@
+//
+//  TimerModel.swift
+//  HandwritingTimer
+//
+//  Created by MURAKAMI on 2018/01/28.
+//  Copyright © 2018年 MURAKAMI. All rights reserved.
+//
+
+import Foundation
--- HandwritingTimer/ViewController.swift
+++ HandwritingTimer/ViewController.swift
@@ -13,10 +13,25 @@ import PopupDialog
 import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var minutes10digitsView: ACEDrawingView!
+    @IBOutlet weak var minutes1digitsView: ACEDrawingView!
+    @IBOutlet weak var seconds10digitsView: ACEDrawingView!
+    @IBOutlet weak var seconds1digitsView: ACEDrawingView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        minutes10digitsView.backgroundColor = .white
+        minutes1digitsView.backgroundColor = .white
+        seconds10digitsView.backgroundColor = .white
+        seconds1digitsView.backgroundColor = .white
+
+//        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.jpg")!).withAlphaComponent(0.1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +40,15 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: ACEDrawingViewDelegate {
+    func drawingView(_ view: ACEDrawingView!, willBeginDrawUsing tool: ACEDrawingTool!) {
+        
+    }
+    
+    func drawingView(_ view: ACEDrawingView!, didEndDrawUsing tool: ACEDrawingTool!) {
+        
+    }
 }
 
